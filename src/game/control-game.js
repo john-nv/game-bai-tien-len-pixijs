e90fcfb4e185bcb05e28f24fa33fd52a458d379b
Quang Ngô Văn <quang@sanbul.net>
2023-07-27 17:20:42 +0700

    add spin

--- src/game/control-game.js
+++ src/game/control-game.js
@@ -16,6 +16,8 @@ let scaleRatio = 1;
 let background_control_friend
 let background_control_search
 let background_control_start
+let btnSpin_one
+let btnSpin_two
 let img_friends
 let img_search
 let textStyle
@@ -23,6 +25,7 @@ let text_friends
 let text_search
 let btn_test_game
 let text_test_game
+let text_countdown_spin
 let container
 let app
 
@@ -43,17 +46,41 @@ function _createBtn() {
     const blurFilter = new PIXI.BlurFilter();
     blurFilter.blur = 1;
     const texturePlayingScreen = PIXI.Texture.from('assets/image/playingScreen.png');
+    const homeScreen = PIXI.Texture.from('assets/image/homeScreen.png');
 
     const btn_Search_crop = new PIXI.Rectangle(0, 1042, 140, 105);
+    const spin_crop_one = new PIXI.Rectangle(1550, 743, 240, 96);
+    const spin_crop_two = new PIXI.Rectangle(826, 85, 240, 96);
     const btn_friend_crop = new PIXI.Rectangle(0, 1147, 147, 105);
     const bg_btn_crop = new PIXI.Rectangle(709, 217, 250, 300);
 
     const btnSearch = new PIXI.Texture(texturePlayingScreen.baseTexture, btn_Search_crop);
+    const btnSpinOne = new PIXI.Texture(homeScreen.baseTexture, spin_crop_one);
+    const btnSpinTwo = new PIXI.Texture(homeScreen.baseTexture, spin_crop_two);
     const btnFriend = new PIXI.Texture(texturePlayingScreen.baseTexture, btn_friend_crop);
     const backgroundBtnFriend = new PIXI.Texture(texturePlayingScreen.baseTexture, bg_btn_crop);
     const backgroundBtnSearch = new PIXI.Texture(texturePlayingScreen.baseTexture, bg_btn_crop);
 
+    // ============================== >>> SPIN ==============================
+    btnSpin_one = new CreateButtonSpriteImage(
+        btnSpinOne,
+        scaleRatio,
+        0.5,
+        screenWidth / 2,
+        screenHeight - (btnSpinOne.height / 2),
+        false,
+    )
 
+
+    btnSpin_two = new CreateButtonSpriteImage(
+        btnSpinTwo,
+        scaleRatio,
+        0.5,
+        screenWidth / 2,
+        screenHeight - (btnSpinOne.height / 2),
+        false,
+    )
+    // ============================== SPIN <<< ==============================
     background_control_friend = new CreateButtonSpriteImage(
         backgroundBtnFriend,
         scaleRatio,
@@ -105,6 +132,7 @@ function _createBtn() {
         screenHeight / 2,
     )
 
+    // ============================ >>> TEXT ============================
     textStyle = new PIXI.TextStyle({
         fontFamily: 'Bungee Spice',
         fontSize: scaleRatio * 22,
@@ -115,6 +143,12 @@ function _createBtn() {
     text_friends = new PIXI.Text('Play friends', textStyle);
     text_search = new PIXI.Text('Find a table', textStyle);
     text_test_game = new PIXI.Text('test game', textStyle);
+    text_countdown_spin = new PIXI.Text('--:--', textStyle);
+
+    text_countdown_spin.alpha = 1;
+    text_countdown_spin.anchor.set(0.5);
+    text_countdown_spin.x = btnSpin_one.x;
+    text_countdown_spin.y = screenHeight - (btnSpinOne.height / 4);
 
     text_test_game.alpha = 1;
     text_test_game.anchor.set(0.5);
@@ -139,8 +173,11 @@ function _createBtn() {
     containerControl.addChild(img_friends);
     containerControl.addChild(img_search);
     containerControl.addChild(btn_test_game);
-
+    containerControl.addChild(btnSpin_one)
+    containerControl.addChild(btnSpin_two)
+    containerControl.addChild(text_countdown_spin)
     _eventControl()
+    _autoSpin()
 }
 
 function _eventControl() {
@@ -152,8 +189,9 @@ function _eventControl() {
             containerControl.removeChild(text_search);
         }))
         .on('pointerdown', onButtonHandle(img_search, true, 0.1, scaleRatio * 0.95, scaleRatio * 0.95, 1, '', () => {
-            removeContainertWithAnimation(containerControl)
-            SearchTable(container, app, scaleRatio)
+            // removeContainertWithAnimation(containerControl)
+            // SearchTable(container, app, scaleRatio)
+            alert('find a table')
         }))
         .on('pointerup', onButtonHandle(img_search, true, 0.1, scaleRatio, scaleRatio, 0, undefined))
         .on('pointerupoutside', onButtonHandle(img_search, true, 0.1, scaleRatio, scaleRatio, 0, undefined))
@@ -165,7 +203,9 @@ function _eventControl() {
         .on('pointerout', onButtonHandle(img_friends, true, 0.1, scaleRatio, scaleRatio, 0.2, 'btn_hover', () => {
             containerControl.removeChild(text_friends);
         }))
-        .on('pointerdown', onButtonHandle(img_friends, true, 0.1, scaleRatio * 0.95, scaleRatio * 0.95, 1, ''))
+        .on('pointerdown', onButtonHandle(img_friends, true, 0.1, scaleRatio * 0.95, scaleRatio * 0.95, 1, '', () => {
+            alert('enter id or create a game table with friends')
+        }))
         .on('pointerup', onButtonHandle(img_friends, true, 0.1, scaleRatio, scaleRatio, 0, undefined))
         .on('pointerupoutside', onButtonHandle(img_friends, true, 0.1, scaleRatio, scaleRatio, 0, undefined))
 
@@ -186,4 +226,36 @@ function _eventControl() {
                 }
             });
         }))
+}
+
+function _autoSpin() {
+    let isBtnOneVisible = true;
+    let remainingTime = 10 * 60;
+    const toggleInterval = 400;
+
+    function displayTime() {
+        const minutes = Math.floor(remainingTime / 60);
+        const seconds = remainingTime % 60;
+        text_countdown_spin.text = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+    }
+
+    function toggleSprites() {
+        btnSpin_one.visible = !isBtnOneVisible;
+        btnSpin_two.visible = isBtnOneVisible;
+        isBtnOneVisible = !isBtnOneVisible;
+    }
+
+    setInterval(toggleSprites, toggleInterval);
+
+    const countdownInterval = setInterval(() => {
+        remainingTime--;
+        displayTime();
+        if (remainingTime <= 0) {
+            clearInterval(countdownInterval);
+            text_countdown_spin.text = `done`;
+        }
+    }, 1000);
+
+    toggleSprites();
+    displayTime();
 }
